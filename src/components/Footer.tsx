01e7e0aca2ea2681d0144e41d26757509ecf501d
resident7709 <resident7709@gmail.com>
2024-07-25 13:37:12 +0300

    Project structure, frontend, pages, components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='mt-auto text-center text-zinc-400 py-5 px-7 border-t'>
+      <small>Done by Alex Roma &#x262F; 2024</small>
+    </footer>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+const navLinks = [
+  { href: '/', label: 'Home' },
+  { href: '/posts', label: 'Posts' },
+];
+
+export default function Header() {
+  const pathname = usePathname();
+
+  console.log(pathname);
+
+  return (
+    <header className='flex justify-between items-center py-4 px-3 border-b'>
+      <Link href='/'>
+        <Image
+          src='https://bytegrad.com/course-assets/youtube/example-logo.png'
+          alt='Logo'
+          className='w-[35px] h-[35px]'
+          width={35}
+          height={35}
+        />
+      </Link>
+      <nav>
+        <ul className='flex gap-x-5 text-[14px]'>
+          {navLinks.map(link => (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={`${
+                  pathname === link.href ? 'text-zinc-900' : 'text-zinc-400'
+                }`}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
