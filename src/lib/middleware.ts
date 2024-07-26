886b1d73246db3fb9207d7311f81c243924eb8fe
resident7709 <resident7709@gmail.com>
2024-07-26 11:11:34 +0300

    Edit middleware.ts

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,10 +1,11 @@
-import { NextRequest } from "next/server";
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+// ?? взаимодействие с Kinde_Auth
+import { NextRequest } from 'next/server';
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
 
 export default function middleware(req: NextRequest) {
   return withAuth(req);
 }
 
 export const config = {
-  matcher: ["/create-post"],
+  matcher: ['/create-post'],
 };
