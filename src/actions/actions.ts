43a49c501f171235b95db9cfeb2436001708c05b
resident7709 <resident7709@gmail.com>
2024-07-26 21:22:47 +0300

    Deploy on Vercel

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,23 +1,21 @@
 'use server';
 
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-import prisma from "@/lib/db";
+import prisma from '@/lib/db';
 
 export async function createPost(formData: FormData) {
   // * Check if user is authenticated
-  const {isAuthenticated} = getKindeServerSession()
+  const { isAuthenticated } = getKindeServerSession();
 
   if (!(await isAuthenticated())) {
-    redirect('/api/auth/login');    
+    redirect('/api/auth/login');
   }
 
   const title = formData.get('title') as string;
   const body = formData.get('body') as string;
-
-  console.log(title, body);
 
   // * Insert into database
   await prisma.post.create({ data: { title, body } });
